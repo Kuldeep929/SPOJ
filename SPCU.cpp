9b9bbc5a8bd703bbe7ede6b8808b5c49aa1fb87c
Kuldeep Pancholi <pancholi.kp09@gmail.com>
2020-11-07 10:58:20 +0530

    12

---
+++ SPCU.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;cin >> t;
+    while(t--){
+        int n; cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin >> arr[i];
+        int flag=1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>i){
+                flag=0;
+                break;
+            }   
+        }
+        (flag)?cout << "YES\n":cout<< "NO\n";
+    }
+    return 0;
+}
